fa15313e8b83cd056058171807fc47f472f11dce
abhimakulshrestha <abhimakulshrestha2004@gmail.com>
2023-10-05 22:10:09 +0530

    largest number

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value for a : ");
+        int a = sc.nextInt();
+        System.out.print("Enter value for b : ");
+        int b = sc.nextInt();
+        System.out.print("Enter value for c : ");
+        int c = sc.nextInt();
+
+        int max = a;
+
+        if (b > max){
+            max= b;
+        }
+        if (c > max){
+            max = c ;
+        }
+        System.out.println("Max = "+ max);
+    }
+
+}
